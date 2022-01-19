0f7e26b308fdda1d7d8ff5204d549def0405a70f
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-01-19 22:19:36 +0900

    feat: define json_converters

--- lib/src/json_converter/json_converters.dart
+++ lib/src/json_converter/json_converters.dart
@@ -14,8 +14,11 @@ class PassthroughConverter<T> implements JsonConverter<T, Object?> {
   Object? toJson(T object) => object;
 }
 
-class AutoDateTimeStringConverter implements JsonConverter<String?, dynamic> {
-  const AutoDateTimeStringConverter();
+/// Firestore ドキュメントには FieldValue.serverTimestamp() を、
+/// Dart のインスタンスとしては toIso8601String() した文字列を与える
+/// タイムスタンプのコンバータ
+class AutoTimestampIso8601StringConverter implements JsonConverter<String?, dynamic> {
+  const AutoTimestampIso8601StringConverter();
 
   @override
   String? fromJson(dynamic json) {
@@ -30,4 +33,18 @@ class AutoDateTimeStringConverter implements JsonConverter<String?, dynamic> {
   @override
   dynamic toJson(String? object) =>
       object == null ? FieldValue.serverTimestamp() : Timestamp.fromDate(DateTime.parse(object));
+}
+
+/// エポック秒で int の値を与えるコンバータ
+/// 後から追加すると数が大きくなる表示順序などのフィールド (e.g. order) などに使用する
+class AutoSecondsSinceEpochConverter implements JsonConverter<int?, int> {
+  const AutoSecondsSinceEpochConverter();
+
+  @override
+  int? fromJson(int json) => json;
+
+  /// toJson、つまり、書き込みのためにインスタンスを生成してドキュメント化する時は
+  /// 例えば 2022-01-01 12:00:00 のときは、20220101120000 に置換した整数を保存する
+  @override
+  int toJson(int? object) => object ?? DateTime.now().millisecondsSinceEpoch * 1000;
 }
